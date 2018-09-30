ff95ae019f05fad1e86420d72e722d5766e87778
Stiven <strujillo1293@gmail.com>
2018-09-30 14:03:20 -0400

    Agregada calcPropina plana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calcPropina';
+  title = 'Calculadora de Propinas';
+  propina = 0;
+  porcentaje = 0.1;
 }
